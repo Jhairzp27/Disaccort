764fe39b49d17afa85fec502828009fbe232c8ee
Jhairzp_27 <jhairzp27@hotmail.com>
2024-04-22 17:52:15 -0500

    Add services and change about us

--- JS/script.js
+++ JS/script.js
@@ -66,7 +66,7 @@ prev.onclick = function(){
 // auto run slider
 let refreshInterval = setInterval(() => {
     next.click();
-}, 5000)
+}, 9000)
 // Function to show slider
 function showSlider(keepMenuOpen = false){
     // console.log('showSlider() function called');
@@ -94,7 +94,7 @@ function handleThumbnailClick(index) {
     showSlider(true); // Set keepMenuOpen to true
     refreshInterval = setInterval(() => { // Restart interval after clicking thumbnail
         next.click();
-    }, 5000)
+    }, 9000)
 }
 
 // Click event listener for thumbnails
@@ -104,7 +104,137 @@ thumbnails.forEach((thumbnail, index) => {
     })
 })
 
+
 //===================FOOTER==========
 var yearElement = document.getElementById('year');
 var currentYear = new Date().getFullYear();
 yearElement.textContent = '@' + currentYear + '| Todos los derechos reservados';
+
+
+//==============REDIRECCION SUAVE===================
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    links.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
+
+//======================ABOUT US==========================
+document.addEventListener('scroll', () => {
+    const aboutSection = document.querySelector('.about-section');
+    const cardsContainer = document.querySelector('.cards-container');
+    const scrollImage = document.querySelector('.scroll-image');
+    const scrollImageText = document.querySelector('.scroll-image-text');
+    const sectionTop = aboutSection.offsetTop;
+    const sectionHeight = aboutSection.offsetHeight;
+    const scrollPosition = window.scrollY + window.innerHeight;
+
+    // Define la posición de desplazamiento deseada para las tarjetas
+    const desiredScrollPositionCards = sectionTop + (sectionHeight * 0.8);
+
+    // Define la posición de desplazamiento deseada para la imagen
+    const desiredScrollPositionImage = sectionTop + (sectionHeight * 1.15);
+
+    // Verificar si el usuario ha llegado a la posición deseada para las tarjetas
+    if (scrollPosition > desiredScrollPositionCards) {
+        aboutSection.classList.add('show');
+        cardsContainer.classList.add('show');
+    } else {
+        aboutSection.classList.remove('show');
+        cardsContainer.classList.remove('show');
+    }
+
+    // Verificar si el usuario ha llegado a la posición deseada para la imagen
+    if (scrollPosition > desiredScrollPositionImage) {
+        scrollImage.style.opacity = '0.2';
+        scrollImageText.style.opacity = '1';
+    } else {
+        scrollImage.style.opacity = '1';
+        scrollImageText.style.opacity = '0';
+    }
+
+    // Añadir clase 'scrolled' al hacer scroll
+    if (window.scrollY > 50) {
+        aboutSection.classList.add('scrolled');
+    } else {
+        aboutSection.classList.remove('scrolled');
+    }
+});
+
+
+document.addEventListener('scroll', () => {
+    const aboutSection = document.querySelector('.about-section');
+    const sectionTitle = document.querySelector('.section-title');
+    const sectionTop = aboutSection.offsetTop;
+    const scrollPosition = window.scrollY;
+
+    // Añadir clase 'scrolled' al hacer scroll dentro de la sección 'About' o antes de ella
+    if (scrollPosition >= sectionTop) {
+        aboutSection.classList.add('scrolled');
+        sectionTitle.classList.add('scrolled'); // Agregar clase al título
+    } else {
+        aboutSection.classList.remove('scrolled');
+        sectionTitle.classList.remove('scrolled'); // Quitar clase del título
+    }
+});
+
+
+// =====================SERVICES======================
+document.addEventListener('DOMContentLoaded', function () {
+    const timelineItems = document.querySelectorAll('.timeline-item');
+
+    let lastScrollTop = 0;
+    let visibleIndex = 0;
+
+    function checkTimelineItems() {
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollTop > lastScrollTop) {
+            // Scroll hacia abajo
+            if (visibleIndex < timelineItems.length) {
+                timelineItems[visibleIndex].classList.add('active');
+                visibleIndex++;
+            }
+        } else {
+            // Scroll hacia arriba
+            if (visibleIndex > 0) {
+                visibleIndex--;
+                timelineItems[visibleIndex].classList.remove('active');
+            }
+        }
+
+        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+    }
+
+    // Comprobar al cargar la página
+    checkTimelineItems();
+
+    // Comprobar al hacer scroll
+    window.addEventListener('scroll', debounce(checkTimelineItems, 50)); // Añadimos debounce para mejorar el rendimiento
+});
+
+// Función debounce para mejorar el rendimiento
+function debounce(func, wait) {
+    let timeout;
+    return function executedFunction(...args) {
+        const later = () => {
+            clearTimeout(timeout);
+            func(...args);
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+}
+
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # DISACCORT
 
 Page dynamic
+
+1. Medicion
+2. Cotizacion
+3. Seleccion de cortinas
+4. Instalacion
+5. Mantenimiento o lavado
